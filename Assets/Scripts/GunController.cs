65aa9974472e2427e8114f787f73f3ac017de838
KyleReddeman <kyler7662@gmail.com>
2017-02-28 19:24:17 -0800

    Initial Commit, don't remember whats going on with the project

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+
+    public float speed = 50f;
+	public Transform rotatePoint;
+    
+    private float turnAxisInput;
+
+    void Awake() {
+    }
+
+    void Start () {
+   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		turnAxisInput = -1 * Input.GetAxis("TurretAxis");
+
+	}
+	void LateUpdate() {
+		if(turnAxisInput != 0) {
+
+			rotatePoint.Rotate(turnAxisInput * speed * Time.deltaTime, 0f, 0f);
+			float x = rotatePoint.rotation.eulerAngles.x;
+			x += 180;
+			if(x >= 360) {
+				x -= 360;
+			}
+			rotatePoint.rotation = Quaternion.Euler(Mathf.Clamp(x, 165f, 186f) - 180f, 0f, 0f);//Mathf.Clamp(x, 165f, 186f) + 180, 0f, 0f);
+		}
+
+	}
+
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody rigid;
+	private float moveInput;
+	private float turnInput;
+	public float moveSpeed;
+	public float turnSpeed;
+	public float maxAngle;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveInput = Input.GetAxis("Vertical");
+		turnInput = Input.GetAxis("Horizontal");
+
+	}
+
+	void FixedUpdate() {
+		rigid.AddForce(rigid.transform.forward * moveSpeed * moveInput);
+		rigid.MoveRotation(rigid.rotation * Quaternion.Euler(0f, turnInput * turnSpeed * Time.deltaTime, 0f));
+		Vector3 velocity = rigid.velocity;
+		float angle = Vector3.Angle(velocity.normalized, rigid.transform.forward);
+		if(velocity.magnitude > 5f && angle > maxAngle) {
+			rigid.AddForce(-.8f * rigid.velocity);
+		}
+	}
+}
